8254188e1c41eec823b51358398367c336c63904
Sofiane-Mekdad <116033550+Sofiane-Mekdad@users.noreply.github.com>
2022-11-05 17:13:04 +0100

    reader

---
+++ reader.js
@@ -0,0 +1,21 @@
+
+
+const button = document.getElementById("button")
+const audio = document.getElementById("audio")
+
+let state = "false"
+button.addEventListener("click", function(e){
+  if(state === "false"){
+    const audioContext = new AudioContext()
+    const track = audioContext.createMediaElementSource(audio)
+    track.connect(audioContext.destination)
+    audio.play()
+    state = "played"
+  }else if (state === "played"){
+    audio.pause()
+    state = "paused"
+  }else{
+    audio.resume()
+  }
+  
+})
