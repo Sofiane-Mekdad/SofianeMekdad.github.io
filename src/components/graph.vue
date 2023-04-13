5856c058969fa0b5fef79656cf16ac80390a1cb9
Sofiane-Mekdad <116033550+Sofiane-Mekdad@users.noreply.github.com>
2023-04-13 23:33:06 +0200

    animation premier jet
    ajout d'un watcher pour avoir un event d'animation, essayer maintenant d'avoir un truc plus "joli" pour pouvoir ajouter les nombres (donc probablement besoin d'un defilement lateral pour rendre le truc plus "petit"

--- src/components/graph.vue
+++ src/components/graph.vue
@@ -1,6 +1,15 @@
 <template>
   <svg height="400" width="1200">
-    <polygon :points="graphDraw"></polygon>
+    <polygon :points="new_graphDraw">
+      <animate ref="animation"
+        begin="indefinite"
+        attributeName="points"
+        :from="old_graphDraw"
+        :to="new_graphDraw"
+        dur="0.25s"
+        repeatCount="once"
+        />
+    </polygon>
   </svg>
 </template>
 
@@ -8,39 +17,98 @@
 export default {
   data(){return{
     pointArray : [],
-    datas : []
+    datas : [],
+    oldDatas : [],
+    newDatas : []
   }},
 
   computed: {
-    graphDraw : function(){
-      if(this.arrayMaking.length == 0){
+    new_graphDraw : function(){
+      if(this.new_arrayMaking.length == 0){
         return ""
       }
       let string = "0 400, "
-      for(let i = 0; i<this.arrayMaking.length; i++)
+      for(let i = 0; i<this.new_arrayMaking.length; i++)
       { 
 
-        string = string.concat(this.arrayMaking[i].x, " ", this.arrayMaking[i].y, ", ")
+        string = string.concat(this.new_arrayMaking[i].x, " ", 400-this.new_arrayMaking[i].y, ", ")
       }
       string = string.concat("1200 400")
       return string
     },
 
-    arrayMaking : function(){
-      if(this.datas.length==0){return []}
-      let array = []
-      
-      let widht = 1200/this.datas.length
-      let min = Math.min(...this.datas)
-      let max = Math.max(...this.datas)
+    old_graphDraw : function(){
 
-      for(let i = 0; i<this.datas.length; i++){
+      if(this.old_arrayMaking.length == 0)
+      {
+        let string = ""
+      }
+      let string = "0 400, "
+      for(let i = 0; i<this.old_arrayMaking.length; i++)
+      { 
+
+        string = string.concat(this.old_arrayMaking[i].x, " ", 400-this.old_arrayMaking[i].y, ", ")
+      }
+      string = string.concat("1200 400")
+      return string
+    },
+
+    old_arrayMaking : function(){
+      if(this.oldDatas.length==0)
+      { 
+        let array = []
+        let widht = 1200/this.newDatas.length
+        for(let i = 0; i<this.newDatas.length; i++)
+        {
         array[i] = {
           x: Math.trunc(i*widht),
-          y: Math.trunc((this.datas[i]-min)/ (max-min) * 400)
+          y: 0
+        }
+        }
+        return array
+      }
+      let array = []
+      
+      let widht = 1200/this.oldDatas.length
+      let min = Math.min(...this.oldDatas)
+      let max = Math.max(...this.oldDatas)
+
+      for(let i = 0; i<this.oldDatas.length; i++){
+        array[i] = {
+          x: Math.trunc(i*widht),
+          y: Math.trunc((this.oldDatas[i]-min)/ (max-min) * 400)
         }
       }
       return array 
+    },
+
+        new_arrayMaking : function(){
+      if(this.newDatas.length==0){return []}
+      let array = []
+      
+      let widht = 1200/this.newDatas.length
+      let min = Math.min(...this.newDatas)
+      let max = Math.max(...this.newDatas)
+
+      for(let i = 0; i<this.newDatas.length; i++){
+        array[i] = {
+          x: Math.trunc(i*widht),
+          y: Math.trunc((this.newDatas[i]-min)/ (max-min) * 400)
+        }
+      }
+      return array 
+    }
+  },
+
+  watch:{
+    datas : {
+      handler(value){
+        this.oldDatas = this.newDatas
+        this.newDatas = value
+
+        this.$refs.animation.beginElement()
+      },
+      deep: true
     }
   }
 }
